15012c57165c3306c8a857b05cdddb564d50dd68
ZF Creators <82653292+ZF-Creators@users.noreply.github.com>
2025-07-07 14:41:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+from backend.symptom_checker import predict_disease
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    probabilities = None
+    if request.method == 'POST':
+        symptoms = request.form.getlist('symptoms')
+        result, probabilities = predict_disease(symptoms)
+    return render_template('index.html', result=result, probs=probabilities)
+
+if __name__ == '__main__':
+    app.run(debug=True)
